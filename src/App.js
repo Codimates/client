471a8ac5b82cb00388b4d07cf97504527a776ace
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-11-16 01:19:39 +0530

    Protected Router

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import {Toaster} from 'react-hot-toast'
+import UserContextProvider from './context/UserContext';
 import HomePage from './pages/HomePage';
 import SignPage from './pages/SignandRegPage/SignPage';
 import RegisterPage from './pages/SignandRegPage/RegisterPage';
 import LandingPage from './pages/customerPages/LandingPage';
+import ProtectedRoute from './protectedRouters/ProtectedRoute';
+
 
 
 axios.defaults.baseURL = 'http://localhost:4000';
@@ -16,6 +19,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <UserContextProvider>
         <Toaster position='bottom-right' toastOptions={{duration: 3000}}></Toaster>
         <Routes>
 
@@ -23,10 +27,11 @@ function App() {
           <Route path='/signin' element={<SignPage/>}/>
           <Route path='/register' element={<RegisterPage/>}/>
 
-          <Route path='/home' element={<LandingPage/>}/>
+          <Route path='/home' element={<ProtectedRoute><LandingPage/></ProtectedRoute>}/>
           
           
         </Routes>
+      </UserContextProvider>
       </BrowserRouter>
     </div>
   );
