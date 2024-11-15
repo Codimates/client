a7e27ff045345b1cded8fc498db7d6c490d72c98
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-11-15 23:28:26 +0530

    login

--- src/components/userRegAndSignCompo/SignCompo.js
+++ src/components/userRegAndSignCompo/SignCompo.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { FaEnvelope, FaLock, FaSignInAlt, FaUserPlus } from 'react-icons/fa';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignCompo() {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const logingUser = async (e) => {
+    e.preventDefault();
+    const { email, password } = data;
+    try {
+      const response = await axios.post('/login', { email, password });
+      const userData = response.data;
+      if (userData.error) {
+          toast.error(userData.error);
+      } else {
+          setData({ email: '', password: '' }); // Clear input fields
+          localStorage.setItem('user', JSON.stringify(userData));
+          const role = userData.role;
+          switch (role) {
+              case 'customer':
+                  navigate('/home');
+                  break;
+              default:
+                  toast.error('Invalid role');
+          }
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  }
+  
   return (
     <div className='flex items-center justify-center min-h-screen'>
       <div className='w-full max-w-md p-8 space-y-6 bg-[#19191A] rounded-lg shadow-md border border-orange-500'>
@@ -10,7 +44,7 @@ export default function SignCompo() {
         <h2 className='text-2xl font-semibold text-center text-white'>Sign In</h2>
         
         {/* Form */}
-        <form className='space-y-4'>
+        <form className='space-y-4' onSubmit={logingUser}>
           {/* Email Input */}
           <div>
             <label htmlFor="email" className='block text-sm font-medium text-white text-start'>
@@ -24,6 +58,8 @@ export default function SignCompo() {
                 id="email"
                 placeholder="Enter your email"
                 className='w-full pl-10 pr-4 py-2 text-sm text-white bg-[#19191A] border border-gray-600 rounded-lg focus:outline-none focus:border-yellow-500 h-12'
+                value={data.email}
+                onChange={(e) => setData({ ...data, email: e.target.value })}
               />
             </div>
           </div>
@@ -41,6 +77,8 @@ export default function SignCompo() {
                 id="password"
                 placeholder="Enter your password"
                 className='w-full pl-10 pr-4 py-2 text-sm text-white bg-[#19191A] border border-gray-600 rounded-lg focus:outline-none focus:border-yellow-500 h-12'
+                value={data.password}
+                onChange={(e) => setData({ ...data, password: e.target.value })}
               />
             </div>
           </div>
